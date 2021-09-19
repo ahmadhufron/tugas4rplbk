c639247a4a10d7b1d5e007eb91098339935d42a8
Alfa Husni Mubarok <alfahusnimubarok@gmail.com>
2021-09-19 08:40:00 +0700

    03. Merapikan dan menambahkan kode - Alfa
    Merapikan kode pada file App.js kemudian juga menambahkan kode untuk mengimport dan memanggil header dan footer dalam App.js ini.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./index.css";
 import { useState, useEffect, createContext,useContext } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const themes = {
   light: {
@@ -12,10 +14,11 @@ const themes = {
     text: "#fff",
   },
 };
+export const AppsContext = React.createContext({});
 const ThemeContext = createContext();
 export default function App() {
   // penggunaan useState1
-  const [values, setValues] = useState({
+  const [values, setValues, apps] = useState({
     firstName: '',
     lastName: '',
     email: '',
@@ -28,7 +31,7 @@ export default function App() {
   // penggunaan useState3
   const [valid, setValid] = useState (false);
 
-// penggunaan useState4
+  // penggunaan useState4
   const [valueTheme, setValueTheme] = useState(themes.light);
 
   const handleFirstNameInputChange = (event) => {
@@ -77,76 +80,76 @@ export default function App() {
     }
   }, [values.password]);
 
-
+   
   return (
-    
-    <div class="form-container" style={{ backgroundColor: valueTheme.background }}>
-      <form class="register-form" onSubmit={handleSubmit}>
-        {/* Uncomment the next line to show the success message */}
-       { submitted && valid ?  <div class="success-message">Success! Thank you for registering {values.lastName}</div> : null }
-       <h2 style = {{ fontFamily:'Roboto',color: valueTheme.text, textAlign:'center'}}>Form Pendaftaran Praktikum</h2>
-        <input
-          id="first-name"
-          class="form-field"
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-          value = {values.firstName}
-          onChange={handleFirstNameInputChange} 
-        />
-        {/* Uncomment the next line to show the error message */}
-        {submitted && !values.firstName ? <span id="first-name-error">Please enter a first name</span> : null}
-        <input
-          id="last-name"
-          class="form-field"
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          value = {values.lastName}
-          onChange={handleLastNameInputChange}
-        />
-        {/* Uncomment the next line to show the error message */}
-       {submitted && !values.lastName ?  <span id="last-name-error">Please enter a last name</span> : null}
-        <input
-          id="email"
-          class="form-field"
-          type="text"
-          placeholder="Email"
-          name="email"
-          value = {values.email}
-          onChange={handleEmailInputChange}
-        />
-        {/* Uncomment the next line to show the error message */}
-        {submitted && !values.email ? <span id="email-error">Please enter an email address</span> : null}
-        
-        <input
-          id="password"
-          class="form-field"
-          type="password"
-          placeholder="Your Password"
-          name="password"
-          value = {values.password}
-          onChange={handlePasswordInputChange}
-        />
-        {/* Uncomment the next line to show the error message */}
-       {submitted && !values.password ?  <span id="password-error">Please enter a password</span> : null}       
-        <button class="form-field button" type="submit">
+    <AppsContext.Provider value={{ apps }}>
+      <Header />
+      <div class="form-container" style={{ backgroundColor: valueTheme.background }}>
+        <form class="register-form" onSubmit={handleSubmit}>
+          {/* Uncomment the next line to show the success message */}
+          { submitted && valid ?  <div class="success-message">Success! Thank you for registering {values.lastName}</div> : null }
+          <h2 style = {{ fontFamily:'Roboto',color: valueTheme.text, textAlign:'center'}}>Form Pendaftaran Praktikum</h2>
+          <input
+            id="first-name"
+            class="form-field"
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            value = {values.firstName}
+            onChange={handleFirstNameInputChange} 
+          />
+          {/* Uncomment the next line to show the error message */}
+          {submitted && !values.firstName ? <span id="first-name-error">Please enter a first name</span> : null}
+          <input
+            id="last-name"
+            class="form-field"
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            value = {values.lastName}
+            onChange={handleLastNameInputChange}
+          />
+          {/* Uncomment the next line to show the error message */}
+          {submitted && !values.lastName ?  <span id="last-name-error">Please enter a last name</span> : null}
+          <input
+            id="email"
+            class="form-field"
+            type="text"
+            placeholder="Email"
+            name="email"
+            value = {values.email}
+            onChange={handleEmailInputChange}
+          />
+          {/* Uncomment the next line to show the error message */}
+          {submitted && !values.email ? <span id="email-error">Please enter an email address</span> : null}
+          <input
+            id="password"
+            class="form-field"
+            type="password"
+            placeholder="Your Password"
+            name="password"
+            value = {values.password}
+            onChange={handlePasswordInputChange}
+          />
+          {/* Uncomment the next line to show the error message */}
+          {submitted && !values.password ?  <span id="password-error">Please enter a password</span> : null}
+          <button class="form-field button" type="submit">
           Register
-        </button>
-      
+          </button>
       </form>
       <ThemeContext.Provider value={valueTheme}>
-      <Content tema={valueTheme} />
+        <Content tema={valueTheme} />
         <button class="form-field1 button1" onClick={() =>setValueTheme(
-              valueTheme === themes.light ? themes.dark : themes.light
-            )
-          }>
+          valueTheme === themes.light ? themes.dark : themes.light
+          )}>
           Change Color 
         </button>
         </ThemeContext.Provider>
     </div>
-  
+    <Footer />
+    </AppsContext.Provider>
   );
+  
   function Content(props) {
     return (
       <div>
@@ -169,5 +172,6 @@ export default function App() {
          Warna Form {theme.background}
         </p>
     );
+   
   }
 }
